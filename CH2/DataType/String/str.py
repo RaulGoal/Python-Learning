7b42be0df683cb10b8a443e135f66b7f5a46da82
Raul <q1231145@gmail.com>
2021-04-13 05:54:05 -0700

    This exapmel shows simple operation on string data type.

---
+++ CH2/DataType/String/str.py
@@ -0,0 +1,15 @@
+first_name = "raul"
+last_name = "chen"
+full_name = first_name + " "+last_name 
+
+print(full_name) # output: raul chen
+
+
+print(full_name.title()) # output: Raul Chen
+
+print(full_name.upper()) # output: RAUL CHEN
+
+message = "PYTHON" 
+
+print(message.lower()) # output: python
+
